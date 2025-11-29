3cbd028002b2886d7f0e9167023de7e4f9ef42c6
Masaaki Izumi <m_asa--uzi0147@nifty.com>
2025-11-29 19:47:59 +0900

    スマホの画面向きを縦に固定

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';              // ★ 追加
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'input_storage.dart';
 import 'screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // ★ スマホの画面向きを縦に固定
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   // SharedPreferences 初期化
   final storage = await InputStorage.init();
